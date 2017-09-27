d9b1332c99c1a19b3b609ca464a7045c1551f1cf
joe9724 <64060764@qq.com>
2017-09-27 23:01:02 +0800

    Update Main.go

--- Main.go
+++ Main.go
@@ -7,6 +7,8 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Sign struct {
@@ -42,6 +44,7 @@ var db *sql.DB
 
 func init() {
 	db, _ = sql.Open("mysql", "root:root811123@tcp(106.14.2.153:3306)/help")
+
 	db.SetMaxOpenConns(20000)
 	db.SetMaxIdleConns(10000)
 	db.Ping()
@@ -54,12 +57,61 @@ func main() {
 
 func startHttpServer() {
 	http.HandleFunc("/sign", _Sign)
+	http.HandleFunc("/collection", _Collection)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
+//
+func _Collection(w http.ResponseWriter, r *http.Request) {
+
+	rows, err := db.Query("select id,DispeopleId,AccessRecord,Photos,Latitude,Longitude,Location,AccessTime from collection")
+	checkErr(err)
+	var Arr []Collection
+	for rows.Next() {
+		var Id int
+		var DispeopleId string
+		var AccessRecord string
+		var Photos string
+		var Latitude float64
+		var Longitude float64
+		var Location string
+		var AccessTime string
+
+		err = rows.Scan(&Id, &DispeopleId, &AccessRecord, &Photos, &Latitude, &Longitude, &Location, &AccessTime)
+		checkErr(err)
+
+		var __col Collection
+		__col.Id = Id
+		__col.DispeopleId = DispeopleId
+		__col.AccessRecord = AccessRecord
+		__col.Photos = Photos
+		__col.Latitude = Latitude
+		__col.Longitude = Longitude
+		__col.Location = Location
+		__col.AccessTime = AccessTime
+
+		Arr = append(Arr, __col)
+
+	}
+
+	//
+	data, err := json.Marshal(Arr)
+	if err != nil {
+		log.Fatal("err get data: ", err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Server", "NanjingYouzi")
+	defer func() {
+		io.WriteString(w, string(data))
+		//fmt.Print(string(data))
+		//w.Write(data)
+	}()
+}
+
+//签到
 func _Sign(w http.ResponseWriter, r *http.Request) {
 
 	rows, err := db.Query("select id,workerId,workerName,latitude,longitude,location,workTime from sign")
